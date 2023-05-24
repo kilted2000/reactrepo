3bc5744e763287e80379a1acd2f066c1b099d9c2
Richard Whittington <53500493+kilted2000@users.noreply.github.com>
2023-05-24 02:28:36 +0000

    add here

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
-      <h2>Start to see the magic happen!</h2>
+      <h2>Start here to see the magic happen!</h2>
     </div>
   );
 }
